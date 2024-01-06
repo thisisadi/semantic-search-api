6e4892f93fe6bf1d56733adf1dc0900f3805c98d
Aditya Jha <adityajha010202@gmail.com>
2024-01-06 18:51:30 +0530

    added routes for projects search and bad requests

--- index.ts
+++ index.ts
@@ -12,6 +12,10 @@ async function startServer() {
     app.use(cors());
     app.use(express.json());
     app.use('/', recordsRouter);
+    app.use('/search', recordsRouter);
+    app.use((req, res) => {
+        res.status(404).send('Not Found');
+    });
 
     app.listen(PORT, () => {
         console.log(`Server is running on http://localhost:${PORT}`);
